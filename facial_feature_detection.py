ab01db3175b20608dd4ec00154683faaa946166f
B02902063 <fd20090aq@gmail.com>
2020-06-17 05:04:45 +0800

    complete

--- facial_feature_detection.py
+++ facial_feature_detection.py
@@ -1,126 +1,226 @@
 import tensorflow as tf
 from IPython.display import clear_output
 import time
+import cv2
+import numpy as np
+from models.FCN import *
+import os
+import shutil
+import tensorflow_addons as tfa
+import sys
 
 
-EPOCHS = 1000
+overlay_colors = [[0, 0, 255], [0, 255, 0], [255, 0, 0], [255, 255, 0], [0, 255, 255], [255, 0, 255], [192, 192, 192], [128, 0, 0], [0, 128, 0], [255, 255, 255], [128, 0, 128], [0, 128, 128], [0, 0, 128]]
+num_class = 5
+n_m_label = [2, 3]
+
+directory = "helenstar_5_all"
+def my_mkdir(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+"""
+try:
+    shutil.rmtree("./{0}".format(directory))
+except:
+    pass
+"""
+
+my_mkdir("./{0}".format(directory))
+my_mkdir("./{0}/mix_model".format(directory))
+my_mkdir("./{0}/test_proce".format(directory))
+my_mkdir("./{0}/train_proce".format(directory))
+my_mkdir("./{0}/test_proce/pred".format(directory))
+my_mkdir("./{0}/train_proce/pred".format(directory))
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+    
+EPOCHS = 100000
 BUFFER_SIZE = 1000
-BATCH_SIZE = 32
+BATCH_SIZE = 1
 
-chin_model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(200, 200 ,3)),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(1600),
-    tf.keras.layers.Reshape([40, 40])
-])
+model = FCN8s((224,224,3), num_class)
+bgr_mean = np.array([0.406, 0.456, 0.485])
+bgr_std = np.array([0.225, 0.224, 0.229])
 
-nose_model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(200, 200 ,3)),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(1600),
-    tf.keras.layers.Reshape([40, 40])
-])
+def write_processdure(img, label=None, count=0, path="./{0}/test_proce".format(directory), nose_mouth=False, find_suspicious=False):
 
-mouth_model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(200, 200 ,3)),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(1600),
-    tf.keras.layers.Reshape([40, 40])
-])
+    predict = model(img, training=False)
+    if type(predict) == tuple:
+        predict = predict[0]
+    predict = predict.numpy()[0]
+    predict = np.argmax(predict, axis=-1)
+    
+    img = img.numpy()[0]
+    img = (img * bgr_std + bgr_mean) * 255
+    img = img.astype(np.uint8)
+    if label is not None:
+        label = label.numpy()[0]
+    def draw_overlay(img, indices, color):
+        rec_result = img.copy()
+        overlay = img.copy()
+        overlay[indices] = color
+        return cv2.addWeighted(overlay, 0.5, rec_result, 0.5, 0) 
+        
+    predict_img = img.copy()
+    truth_img = img.copy()
+    
+    pred_n_m_count = 0
+    face_count = (img.shape[0] * img.shape[1]) - len(np.where(predict==0)[0])
+    if nose_mouth:
+        for i in [1, 2, 3 ,4]:
+            n_m = np.where(predict==i)
+            predict_img = draw_overlay(predict_img, n_m, overlay_colors[i])
+            if i in n_m_label:
+                pred_n_m_count += len(n_m[0])
+            if label is not None:
+                truth_img = draw_overlay(truth_img, np.where(label==i), [0, 0, 255])
+    else:
+        for i in range(1, num_class):
+            predict_img = draw_overlay(predict_img, np.where(predict==i), overlay_colors[i])
+            if label is not None:
+                truth_img = draw_overlay(truth_img, np.where(label==i), overlay_colors[i])
+    if label is not None:
+        seperate_line = np.zeros(shape=(img.shape[0], 5, img.shape[2]), dtype=np.uint8)
+        seperate_line[..., 1] = 76
+        seperate_line[..., 2] = 153
 
-chin_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-nose_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-mouth_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        rrt = np.concatenate([predict_img, seperate_line, truth_img], axis=1)
+    else:
+        rrt = predict_img
+      
+    if find_suspicious:
+        if face_count == 0:
+            cv2.imwrite("./{0}/pred/{1}_{2}_{3}.jpg".format(path, count, face_count, pred_n_m_count), rrt)
+        elif pred_n_m_count/face_count > 0.04 and pred_n_m_count > 1000:
+            cv2.imwrite("./{0}/suspicious/{1}_{2}_{3}.jpg".format(path, count, round(pred_n_m_count/face_count, 4), pred_n_m_count), rrt)
+        else:
+            cv2.imwrite("./{0}/pred/{1}_{2}_{3}.jpg".format(path, count, pred_n_m_count/face_count, pred_n_m_count), rrt)
+    else:
+        cv2.imwrite("./{0}/pred/{1}.jpg".format(path, count), rrt)
+    return pred_n_m_count
 
 def read_tfrecord(serialized_example):
     # Create a description of the features.
     feature_description = {
         'img': tf.io.FixedLenFeature([], tf.string, default_value=''),
-        'chin_label': tf.io.FixedLenFeature([], tf.string, default_value=''),
-        'nose_label': tf.io.FixedLenFeature([], tf.string, default_value=''),
-        'mouth_label': tf.io.FixedLenFeature([], tf.string, default_value=''),
+        'label': tf.io.FixedLenFeature([], tf.string, default_value='')
     }
     example = tf.io.parse_single_example(serialized_example, feature_description)
     img = tf.io.parse_tensor(example['img'], out_type=tf.int32)
-    chin_label = tf.io.parse_tensor(example['chin_label'], out_type=tf.float32)
-    nose_label = tf.io.parse_tensor(example['nose_label'], out_type=tf.float32)
-    mouth_label = tf.io.parse_tensor(example['mouth_label'], out_type=tf.float32)
-    return img, chin_label, nose_label, mouth_label
+    label = tf.io.parse_tensor(example['label'], out_type=tf.int32)
+    img = tf.cast(img, tf.float32)
+    #label = tf.cast(label, tf.float32)
+    #img /= 127.5
+    #img -= 1.0
+    img = (img / 255. - bgr_mean) / bgr_std
+    #label = tf.concat([tf.ones_like(label)[:, :112], tf.ones_like(label)[:, 112:]*2], axis=1)
+    #label = tf.ones_like(label)
+    img.set_shape((224, 224, 3))
+    label.set_shape((224, 224))
+    return img, label
+
+def read_masked_tfrecord(serialized_example):
+    # Create a description of the features.
+    feature_description = {
+        'img': tf.io.FixedLenFeature([], tf.string, default_value=''),
+    }
+    example = tf.io.parse_single_example(serialized_example, feature_description)
+    img = tf.io.parse_tensor(example['img'], out_type=tf.int32)
+    img = tf.cast(img, tf.float32)
+    img = (img / 255. - bgr_mean) / bgr_std
+    img.set_shape((224, 224, 3))
+    return img
 
 @tf.function
-def train_step(img, chin_label, nose_label, mouth_label):
+def train_step(img, label):
 
     with tf.GradientTape(persistent=True) as tape:
-        chin_predict = chin_model(img)
-        nose_predict = nose_model(img)
-        mouth_predict = mouth_model(img)
-        
-        chin_loss = loss_obj(chin_label, chin_predict)
-        nose_loss = loss_obj(nose_label, nose_predict)
-        mouth_loss = loss_obj(mouth_label, mouth_predict)
+        predict = model(img, training=True)
+
+        loss = tf.nn.sparse_softmax_cross_entropy_with_logits(label, predict)
     
     # Calculate the gradients
-    chin_gradients = tape.gradient(chin_loss, chin_model.trainable_variables)
-    nose_gradients = tape.gradient(nose_loss, nose_model.trainable_variables)
-    mouth_gradients = tape.gradient(mouth_loss, mouth_model.trainable_variables)
+    gradients = tape.gradient(loss, model.trainable_variables)
     
     # Apply the gradients to the optimizer
-    chin_optimizer.apply_gradients(zip(chin_gradients, chin_model.trainable_variables))
-    nose_optimizer.apply_gradients(zip(nose_gradients, nose_model.trainable_variables))
-    mouth_optimizer.apply_gradients(zip(mouth_gradients, mouth_model.trainable_variables))
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
     
-raw_dataset = tf.data.TFRecordDataset("helen.tfrecord")
-dataset = raw_dataset.map(read_tfrecord)
+raw_dataset = tf.data.TFRecordDataset("helenstar_5_categorical.tfrecord")
+#raw_masked_dataset = tf.data.TFRecordDataset("masked.tfrecord")
 
-dataset = dataset.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+train_dataset = raw_dataset.take(1699)
+train_dataset = train_dataset.map(read_tfrecord)
+validation_dataset = raw_dataset.skip(1699)
+validation_dataset = validation_dataset.map(read_tfrecord).batch(1)
 
-checkpoint_path = "./checkpoints"
+dataset = train_dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+#raw_masked_dataset = 
+checkpoint_path = "./model/nose_mouth_eye/nme.ckpt"  #FCN8s_{epoch:04d}-{val_accuracy:.2f}.ckpt
 
-ckpt = tf.train.Checkpoint(chin_model=chin_model,
-                           nose_model=nose_model,
-                           mouth_model=mouth_model,
-                           chin_optimizer=chin_optimizer,
-                           nose_optimizer=nose_optimizer,
-                           mouth_optimizer=mouth_optimizer)
+"""
+ckpt = tf.train.Checkpoint(model=model,
+                           optimizer=optimizer)
 
 ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
 if ckpt_manager.latest_checkpoint:
     ckpt.restore(ckpt_manager.latest_checkpoint)
     print ('Latest checkpoint restored!!')
-for epoch in range(EPOCHS):
-    start = time.time()
+"""    
 
-    n = 0
-    for img, chin_label, nose_label, mouth_label in dataset:
-        train_step(img, chin_label, nose_label, mouth_label)
-        if n % 10 == 0:
-            print ('.', end='')
-        n+=1
-    clear_output(wait=True)
+if len(sys.argv) > 1:       
+    try:
+        loaded_checkpoint_path = tf.train.latest_checkpoint("./model/nose_mouth_eye/")
+        model.load_weights(loaded_checkpoint_path)
+        print("Restore file successfully")
+    except:
+        print("Didn't find checkpoint. Exit.")
+        exit()
+    if sys.argv[1] == "mask":
+        mask_dataset = tf.data.TFRecordDataset("masked224.tfrecord")
+        mask_dataset = mask_dataset.map(read_masked_tfrecord).batch(1)
+        count = 0
+        nm_count = []
+        for img in mask_dataset:
+            nm_count.append(write_processdure(img, None, count, "./{0}/masked_proce".format(directory), nose_mouth=True, find_suspicious=True))
+            count += 1
+        print(sum(nm_count)/count, max(nm_count), min(nm_count))
+    elif sys.argv[1] == "face":
+        face_dataset = tf.data.TFRecordDataset("face224.tfrecord")
+        face_dataset = face_dataset.map(read_masked_tfrecord).batch(1)
+        count = 0
+        nm_count = []
+        for img in face_dataset:
+            nm_count.append(write_processdure(img, None, count, "./{0}/face_proce".format(directory), nose_mouth=True))
+            count += 1
+        print(sum(nm_count)/count, max(nm_count), min(nm_count))
+    else:
+        train_dataset = train_dataset.batch(1)
+        test_dataset = raw_dataset.skip(1699).map(read_tfrecord).batch(1)
+        count = 0
+        for img, label in test_dataset:
+            write_processdure(img, label, count)
+            count += 1
+        count = 0
+        for img, label in train_dataset:
+            write_processdure(img, label, count, "./{0}/train_proce".format(directory))
+            count += 1
+    exit()
+try:    
+    loaded_checkpoint_path = tf.train.latest_checkpoint("./model/nose_mouth_eye/")
+    model.load_weights(loaded_checkpoint_path)
+    print("Restore file successfully")
+except:
+    print("Didn't find checkpoint. Initialize a new model.")
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-4),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])   
+           
+callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, verbose=1, save_weights_only=True, monitor="val_accuracy", save_best_only=True)
 
-    if (epoch + 1) % 5 == 0:
-        ckpt_save_path = ckpt_manager.save()
-        print ('Saving checkpoint for epoch {} at {}'.format(epoch+1,
-                                                         ckpt_save_path))
+def generator(dataset):
+    while True:
+        for i, l in dataset:
+            yield i, l
 
-    print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
-                                                      time.time()-start))
+model.fit(dataset, epochs=1000, callbacks=[callback], validation_data=validation_dataset)              
--- helen_tfrecord_generator.py
+++ helen_tfrecord_generator.py
@@ -1,96 +1,38 @@
 import cv2
 import tensorflow as tf
 import numpy as np
+import os
 
-chin_selected_point = list(range(16, 26)) 
-nose_selected_point = list(range(41, 58))
-mouth_selected_point = list(range(57, 114))
 
 all_img = []
-all_chin_l = []
-all_nose_l = []
-all_mouth_l = []
+all_label = []
 
-for n in range(1, 2331):
 
-    with open("./helen/label/{0}.txt".format(n), "r") as fp:
-        temp = fp.readlines()
-        label = temp[1:]
-        img_name = "./helen/helen/" + temp[0].strip() + ".jpg"
-        
-    img = cv2.imread(img_name)
-
-    def label_process(label):    
-        for i in range(len(label)):
-            temp = label[i].split(",")
-            label[i] = [float(t.strip()) for t in temp]
-        return label
-
-    def preprocess(label, h, w, selected_point):
-        selected_label = []
-        for p in selected_point:
-            if label[p] not in selected_label:
-                selected_label.append(label[p])
-
-        return selected_label
-
-    label = label_process(label)
-    chin_label = preprocess(label, img.shape[0], img.shape[1], chin_selected_point)
-    nose_label = preprocess(label, img.shape[0], img.shape[1], nose_selected_point)
-    mouth_label = preprocess(label, img.shape[0], img.shape[1], mouth_selected_point)
-
-    result = cv2.resize(img, (200,200))
-
-    h_scale = 200/img.shape[0]
-    w_scale = 200/img.shape[1]
-    for i in range(len(label)):
-        label[i] = [label[i][0] * w_scale, label[i][1] * h_scale]
-        
-    twoDLabelChin = np.zeros(shape=(40, 40), dtype=np.float32)
-    try:
-        for label in chin_label:
-            l = [label[0] * w_scale, label[1] * h_scale]
-            #print(l, label, img.shape, h_scale, w_scale)
-            ind = [int(l[0]//5), int(l[1]//5)]
-            twoDLabelChin[ind[0], ind[1]] = 1.0
-            
-        twoDLabelNose = np.zeros(shape=(40, 40), dtype=np.float32)
-        for label in nose_label:
-            l = [label[0] * w_scale, label[1] * h_scale]
-            ind = [int(l[0]//5), int(l[1]//5)]
-            twoDLabelNose[ind[0], ind[1]] = 1.0    
-
-        twoDLabelMouth = np.zeros(shape=(40, 40), dtype=np.float32)
-        for label in mouth_label:
-            l = [label[0] * w_scale, label[1] * h_scale]
-            ind = [int(l[0]//5), int(l[1]//5)]
-            twoDLabelMouth[ind[0], ind[1]] = 1.0
-    except:
+for file in os.listdir("./helenstar_release/train"):
+    if "image" not in file:
         continue
-
-    all_img.append(result)
-    all_chin_l.append(twoDLabelChin)
-    all_nose_l.append(twoDLabelNose)
-    all_mouth_l.append(twoDLabelMouth)
+        
+    image_file = "./helenstar_release/train/" + file
+    label_file = "./helenstar_release/train/" + file.split("image")[0] + "label.png"
+    img = cv2.imread(image_file)
+    label = cv2.imread(label_file, cv2.IMREAD_GRAYSCALE)
     
-    #for label in chin_label:
-    #    l = [label[0] * w_scale, label[1] * h_scale]
-    #    result = cv2.circle(result, (int(l[0]), int(l[1])), 1, (0, 0, 255),  -1)
-    #cv2.imwrite("test3.jpg", result)  
-    #exit()
-dataset = tf.data.Dataset.from_tensor_slices((all_img, all_chin_l, all_nose_l, all_mouth_l))
-#rec_result = result.copy()
-#for l in label:
-#    ind = [int(l[0]//5), int(l[1]//5)]
-#    rec_result = cv2.rectangle(rec_result, (ind[0]*5, ind[1]*5), (ind[0]*5+5, ind[1]*5+5), (0, 0, 255), -1)
-#cv2.imwrite("test2.jpg", rec_result)
+    img = cv2.resize(img, (224, 224))
+    label = cv2.resize(label, (224, 224), interpolation=cv2.INTER_NEAREST)
+    
+    label[np.logical_and.reduce((label != 6, label != 7, label != 8, label != 9, label != 0, label != 4, label != 5))] = 1
+    label[label == 6] = 2 # nose
+    label[label == 7] = 3 # mouth
+    label[label == 8] = 3
+    label[label == 9] = 3
+    label[label == 4] = 4 # eye
+    label[label == 5] = 4
+    all_img.append(img)
+    all_label.append(label)
+    
+dataset = tf.data.Dataset.from_tensor_slices((all_img, all_label))
 
-#selected_point = list(range(16, 26)) + list(range(41, 58)) + list(range(57, 114)) #Chin, Nose, Mouth
-#circle_result = result.copy()
-#for l in label:
-#    circle_result = cv2.circle(circle_result, (int(l[0]), int(l[1])), 1, (0, 0, 255),  -1)
-#cv2.imwrite("test3.jpg", circle_result)  
-def serialize_example(img, chin_label, nose_label, mouth_label):
+def serialize_example(img, label):
 
     def _bytes_feature(value):
         """Returns a bytes_list from a string / byte."""
@@ -106,9 +48,7 @@ def serialize_example(img, chin_label, nose_label, mouth_label):
     # data type.
     feature = {
         'img': _bytes_feature(tf.io.serialize_tensor(img)),
-        'chin_label': _bytes_feature(tf.io.serialize_tensor(chin_label)),
-        'nose_label': _bytes_feature(tf.io.serialize_tensor(nose_label)),
-        'mouth_label': _bytes_feature(tf.io.serialize_tensor(mouth_label))
+        'label': _bytes_feature(tf.io.serialize_tensor(label))
     }
 
     # Create a Features message using tf.train.Example.
@@ -116,12 +56,12 @@ def serialize_example(img, chin_label, nose_label, mouth_label):
     example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
     return example_proto.SerializeToString()
 
-def tf_serialize_example(img, label0, label1, label2):
+def tf_serialize_example(img, label):
     tf_string = tf.py_function(
         serialize_example,
-        (img, label0, label1, label2),  # pass these args to the above function.
+        (img, label),  # pass these args to the above function.
         tf.string)      # the return type is `tf.string`.
     return tf.reshape(tf_string, ()) # The result is a scalar
 serialized_features_dataset = dataset.map(tf_serialize_example)
-writer = tf.data.experimental.TFRecordWriter("helen.tfrecord")
+writer = tf.data.experimental.TFRecordWriter("helenstar_5_categorical.tfrecord")
 writer.write(serialized_features_dataset)
